649e7962951493893f15341adb24bca90e89eb51
santex100 <santex080321@gmail.com>
2023-12-09 18:26:31 +0100

    initail commit

--- email.php
+++ email.php
@@ -1,33 +1,12 @@
 <?php
-$subject = 'You Got Message'; // Subject of your email
-$to = "ikyohiabonaventure@gmail.com";  //Recipient's E-mail
-$emailTo = $_REQUEST['email'];
+$to = "ikyohiabonaventure@gmail.com";
+$subject = "Test Email";
+$message = "This is a test email sent using PHP's mail() function.";
+$headers = "From: sender@example.com";
 
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$phone = $_REQUEST['phone'];
-$msg = $_REQUEST['message'];
-
-$email_from = $name.'<'.$email.'>';
-
-$headers = "MIME-Version: 1.1";
-$headers .= "Content-type: text/html; charset=iso-8859-1";
-$headers .= "From: ".$name.'<'.$email.'>'."\r\n"; // Sender's E-mail
-$headers .= "Return-Path:"."From:" . $email;
-
-$message .= 'Name : ' . $name . "\n";
-$message .= 'Email : ' . $email . "\n";
-$message .= 'Phone : ' . $phone . "\n";
-$message .= 'Message : ' . $msg;
-
-if (@mail($to, $subject, $message, $email_from))
-{
-	// Transfer the value 'sent' to ajax function for showing success message.
-	echo 'sent';
-}
-else
-{
-	// Transfer the value 'failed' to ajax function for showing error message.
-	echo 'failed';
+if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully!";
+} else {
+    echo "Email failed to send.";
 }
 ?>
