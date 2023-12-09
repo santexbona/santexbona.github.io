8a479ad32fd27e9407b1c7e0c4f7eda8a2da18d2
santex100 <santex080321@gmail.com>
2023-12-09 18:19:34 +0100

    initial commit

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 $subject = 'You Got Message'; // Subject of your email
-$to = 'ikyohiabonaventure@gmail.com';  //Recipient's E-mail
+$to = "ikyohiabonaventure@gmail.com";  //Recipient's E-mail
 $emailTo = $_REQUEST['email'];
 
 $name = $_REQUEST['name'];
