f7b9d922240bd9814ab1a219b9d5962391675d4f
santex100 <santex080321@gmail.com>
2023-12-12 16:31:04 +0100

    initial commit

--- email.php
+++ email.php
@@ -1,29 +1,33 @@
 <?php
-$subject = 'from the website'; // Subject of your email
-$to = "ikyohiabonaventure@gmail.com"; // Recipient's E-mail
-
+$subject = 'You Got Message'; // Subject of your email
+$to = 'ikyohiabonaventure@gmail.com';  //Recipient's E-mail
 $emailTo = $_REQUEST['email'];
+
 $name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
 $phone = $_REQUEST['phone'];
 $msg = $_REQUEST['message'];
 
-$email_from = $name . '<' . $email . '>';
-$headers = "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-$headers .= "From: " . $email_from . "\r\n";
-$headers .= "Reply-To: " . $email . "\r\n";
+$email_from = $name.'<'.$email.'>';
 
-$message = 'Name : ' . $name . "\n";
+$headers = "MIME-Version: 1.1";
+$headers .= "Content-type: text/html; charset=iso-8859-1";
+$headers .= "From: ".$name.'<'.$email.'>'."\r\n"; // Sender's E-mail
+$headers .= "Return-Path:"."From:" . $email;
+
+$message .= 'Name : ' . $name . "\n";
 $message .= 'Email : ' . $email . "\n";
 $message .= 'Phone : ' . $phone . "\n";
 $message .= 'Message : ' . $msg;
 
-if (@mail($to, $subject, $message, $headers)) {
-    // Transfer the value 'sent' to ajax function for showing success message.
-    echo 'sent';
-} else {
-    // Transfer the value 'failed' to ajax function for showing error message.
-    echo 'failed';
+if (@mail($to, $subject, $message, $email_from))
+{
+	// Transfer the value 'sent' to ajax function for showing success message.
+	echo 'sent';
+}
+else
+{
+	// Transfer the value 'failed' to ajax function for showing error message.
+	echo 'failed';
 }
 ?>
